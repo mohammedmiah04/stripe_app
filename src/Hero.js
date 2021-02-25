a8b02dd1c003ff0347a41cfe52416f52fdc78b02
MohammedM <nanumiah@nanus-mbp.home>
2021-02-25 17:30:41 +0000

    made website and made header and hero

---
+++ src/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
+
+const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
+
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Millions of companies of all size from startups to fortune 500s use
+            Stripes's software and APIs to access payment's, send payouts and
+            manage their businesses online.
+          </p>
+          <button className="btn">Start Now</button>
+        </article>
+
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
