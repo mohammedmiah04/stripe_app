bb682a0be5f0a00e09aa61904000644816ba3d6c
MohammedM <nanumiah@nanus-mbp.home>
2021-03-02 09:02:12 +0000

    Updates

---
+++ precache-manifest.a5c8422ded387d622e08fafbfe50f443.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd69336b0b40688b8dc8031af32b1c27",
+    "url": "/stripe_app/index.html"
+  },
+  {
+    "revision": "264d873adce5194f82ad",
+    "url": "/stripe_app/static/css/main.ee9c843e.chunk.css"
+  },
+  {
+    "revision": "edd9e45190e60511e373",
+    "url": "/stripe_app/static/js/2.b147998e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripe_app/static/js/2.b147998e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264d873adce5194f82ad",
+    "url": "/stripe_app/static/js/main.3ed83932.chunk.js"
+  },
+  {
+    "revision": "979c8ba96c864e43473e",
+    "url": "/stripe_app/static/js/runtime-main.dcf63b32.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/stripe_app/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/stripe_app/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/stripe_app/static/media/phone.58d7e3d6.svg"
+  }
+]);
