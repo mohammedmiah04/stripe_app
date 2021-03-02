af83ce3bce04fe012043b46bbf00242239cbec11
MohammedM <nanumiah@nanus-mbp.home>
2021-03-02 08:46:32 +0000

    added icons and animation to submenu

--- src/context.js
+++ src/context.js
@@ -7,6 +7,7 @@ export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
   const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: "", links: [] });
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -18,6 +19,8 @@ export const AppProvider = ({ children }) => {
   };
 
   const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
     setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
@@ -36,6 +39,7 @@ export const AppProvider = ({ children }) => {
         closeSubmenu,
         closeSidebar,
         location,
+        page,
       }}>
       {children}
     </AppContext.Provider>
